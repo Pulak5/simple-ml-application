f27d491395be9c6f455a2a929bdcb8daae1c6dd9
Pulak5 <pulak@example.com>
2026-05-17 10:22:56 +0530

    Initial ML app commit

---
+++ application.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+import pickle
+import numpy as np
+
+application = Flask(__name__)
+
+
+
+@application.route("/")
+def home():
+    return "ML App Running"
+
+
+if __name__ == "__main__":
+    application.run(debug=True)
